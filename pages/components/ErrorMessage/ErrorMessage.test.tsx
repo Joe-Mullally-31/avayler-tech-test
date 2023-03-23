1013abacf5510f6260461e982ce23d24f510cd27
Joe-Mullally-31 <88665669+Joe-Mullally-31@users.noreply.github.com>
2023-03-23 01:00:54 +0000

    Delete pages directory

--- pages/components/ErrorMessage/ErrorMessage.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react';
-import { ErrorMessage } from './ErrorMessage';
-
-it('should render as expected', () => {
-  expect(render(<ErrorMessage />)).toMatchSnapshot();
-});
